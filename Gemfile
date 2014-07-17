5b123a61956227eaf1b0e231d379dc22e2368993
roybleas <roybleas@ozemail.com.au>
2014-07-17 10:58:37 +1000

    Genesis

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+gem 'rails', '3.2.1'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+group :development do
+	gem 'sqlite3', '1.3.8'
+end
+
+gem 'sass-rails', '3.2.6'
+gem 'coffee-rails', '3.2.2'
+gem 'uglifier', '2.1.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks','1.1.1'
+gem 'jbuilder', '1.0.2'
+
+group :doc do
+	gem 'sdoc','0.3.20' , require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4e545325a892f3da27cf27bcfde2302816f99de1b6940e4716f7e236026c02281caae8c65699921f5e23ab4cad5a3d05bf88078c86645c001abbbc12e862a99f'
